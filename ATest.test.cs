9646ed0bd00983cdb823f3fabfe4e8f10358a40b
Josh Tompkins <josh@joshtompkins.com>
2020-02-09 17:25:55 -0600

    Clean up test a little bit

--- ATest.test.cs
+++ ATest.test.cs
@@ -4,30 +4,28 @@ namespace okra {
     public class ATest : Okra {
         public ATest() {
             Describe("a thing", () => {
-                string myCoolString = "";
-
                 BeforeAll(() => Console.WriteLine("BeforeAll"));
                 AfterAll(() => Console.WriteLine("AfterAll"));
 
-                BeforeEach(() => myCoolString = "beforeEach");
+                BeforeEach(() => Console.WriteLine("BeforeEach"));
                 AfterEach(() => Console.WriteLine("After Each 1"));
 
                 When("a thing does another thing", () => {
-                    JustBeforeEach(() => myCoolString = "justBeforeEach");
+                    JustBeforeEach(() => Console.WriteLine("JustBeforeEach"));
                     AfterEach(() => Console.WriteLine("After Each 2"));
 
                     Then("another thing happens", () => {
                         //normally we'd assert here, but I don't know how that works
                         // yet, so we're just going to write something to the console
                         // so we know this func got called.
-                        Console.WriteLine(myCoolString);
+                        Console.WriteLine("Then 1");
                     });
 
                     Then("a way different thing happens", () => {
                         //normally we'd assert here, but I don't know how that works
                         // yet, so we're just going to write something to the console
                         // so we know this func got called.
-                        Console.WriteLine(myCoolString);
+                        Console.WriteLine("Then 2");
                     });
                 });
             });
